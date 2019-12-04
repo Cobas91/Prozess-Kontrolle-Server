9e903e0a37a04661ec175cf4a924efa17cd5f3ce
Stephan Gärtner <stephan.gaertner72@outlook.de>
2019-12-04 09:44:12 +0100

    update checkliste add

--- routes/addChecklisteSN.js
+++ routes/addChecklisteSN.js
@@ -1,16 +1,15 @@
 var express = require("express"),
   router = express.Router();
   const db = require("../util/dbConnector")
-  const status = require("../util/statusHandler")
-  
+
   router.post("/add/checklisteSN", async ({body},response) => {
-    console.log(body)
-    console.log("Getting Request on db/add/ChecklisteSN", body)
-    const result = "Erfolg"
+    console.log("Getting Request for Adding Checkliste", body)
+    const result = await db.insert("checklisten", [body]).catch(function (err) {
+      return err
+    });
     response.send({
       result
     });
-    error = false;
   });
 
   module.exports = router;
